bf00ecfb91f48f768c75468a8d6a50e3f9950895
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-07 02:19:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCountresData } from './redux/country/countrySlice';
+import Home from './pages/home';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCountresData());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <h1>METRICS WEBAPP</h1>
+      <Home />
     </div>
   );
 }
