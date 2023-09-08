65b3f779404ef2c3351a19c083d264290ddd7c34
Nwanjoh Nalian Achei <nalianachei2@gmail.com>
2023-09-08 16:23:46 +0100

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,7 +14,7 @@ function Home() {
   };
 
   const filterData = countries.filter((item) =>
-    item.name.toLowerCase().includes(searchinCountry.toLowerCase())
+    (item.name.toLowerCase().includes(searchinCountry.toLowerCase()))
   );
 
   return (
